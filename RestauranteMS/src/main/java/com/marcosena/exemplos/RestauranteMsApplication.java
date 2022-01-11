b328a49d17e7ae0b086f9234129217e64895625e
marcoacsena <marcoacsena@gmail.com>
2022-01-11 17:30:07 -0300

    Segundo commit do projeto restaurante do Marco Sena!

---
+++ RestauranteMS/src/main/java/com/marcosena/exemplos/RestauranteMsApplication.java
@@ -0,0 +1,13 @@
+package com.marcosena.exemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteMsApplication.class, args);
+	}
+
+}
---
+++ RestauranteMS/src/test/java/com/marcosena/exemplos/RestauranteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosena.exemplos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
